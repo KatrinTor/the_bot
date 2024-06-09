60e7f0148d47863904b1e662a8c344d324404c02
Katrin Torsunova <katrin.tep@list.ru>
2024-06-09 22:39:35 +0300

    [TG-3] update naming

--- app/questions_state.py
+++ app/questions_state.py
@@ -1,8 +1,7 @@
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.fsm.context import FSMContext
 
 
-class Question(StatesGroup):
+class THE_ASKER(StatesGroup):
     feel_now = State()
     want_to_feel = State()
     able_to_do = State()
