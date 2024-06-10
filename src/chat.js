0719954da57dcbe1a430c9376522f85b4c080626
Katrin Torsunova <63786538+KatrinTor@users.noreply.github.com>
2024-06-10 23:34:12 +0300

    ci: set openai code review bot with GitHub Actions (#7)
    * add cr.yml to actions
    
    * add chat.js with review settings
    
    * fix action, ignore actions/ and src/ review
    
    * change ignore actions/ to ignore .github/
    
    * add manual run to Code Review Action

---
+++ src/chat.js
@@ -0,0 +1,50 @@
+import {ChatGPTAPI} from 'chatgpt';
+
+export class Chat {
+    private chatAPI: ChatGPTAPI;
+
+    constructor(apikey: string) {
+        this.chatAPI = new ChatGPTAPI({
+            apiKey: apikey,
+            apiBaseUrl:
+                process.env.OPENAI_API_ENDPOINT || 'https://api.proxyapi.ru/openai/v1',
+            completionParams: {
+                model: process.env.MODEL || 'gpt-3.5-turbo',
+                temperature: +(process.env.temperature || 0) || 1,
+                top_p: +(process.env.top_p || 0) || 1,
+                max_tokens: process.env.max_tokens
+                    ? +process.env.max_tokens
+                    : undefined,
+            },
+        });
+    }
+
+    private generatePrompt = (patch: string) => {
+        const answerLanguage = process.env.LANGUAGE
+            ? `Answer me in ${process.env.LANGUAGE},`
+            : '';
+
+        const prompt =
+            process.env.PROMPT ||
+            'Below is a code patch, please help me do a brief code review on it. Any bug risks and/or improvement \s' +
+            'uggestions are welcome: Answer me in Russian';
+
+        return `${prompt}, ${answerLanguage}:
+    ${patch}
+    `;
+    };
+
+    public codeReview = async (patch: string) => {
+        if (!patch) {
+            return '';
+        }
+
+        console.time('code-review cost');
+        const prompt = this.generatePrompt(patch);
+
+        const res = await this.chatAPI.sendMessage(prompt);
+
+        console.timeEnd('code-review cost');
+        return res.text;
+    };
+}
