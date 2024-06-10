f198bdba638dba4567301e7a546aa94b80b70e37
Katrin Torsunova <katrin.tep@list.ru>
2024-06-10 18:52:34 +0300

    add add_question_answer() method

---
+++ app/database/answer.py
@@ -0,0 +1,18 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.database.database import AnswerInfo
+
+load_dotenv()
+engine = create_engine(os.getenv("DATABASE_URL"))
+
+
+def add_question_answer(question_id, answer_text):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    answer = AnswerInfo(question_id=question_id, answer=answer_text)
+    session.add(answer)
+    session.commit()
